c2c40db00d98082a8eed105a1b7ccce0f22784ad
bertoldosi <matheus.bertoldosi@gmail.com>
2022-08-24 12:07:49 -0300

    feat: Implementando titulo da pagina

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,16 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 import { HomeContainer } from "../components/container/Home";
 
 const Home: NextPage = () => {
-  return <HomeContainer />;
+  return (
+    <>
+      <Head>
+        <title>Expense Manager</title>
+      </Head>
+      <HomeContainer />
+    </>
+  );
 };
 
 export default Home;
