8195e452cd535ab8a18bf0fab00d6f2670d82868
bertoldosi <matheus.bertoldosi@gmail.com>
2022-09-20 17:42:34 -0300

    feat: Aumentando paginação

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ export const GET_MONTHS = gql`
         name
         amount
         expirationDate
-        shoppings(first: 500) {
+        shoppings(first: 5000) {
           reference
           description
           amount
