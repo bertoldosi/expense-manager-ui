d5aadc6ce2f8fbc475d15b2a4b2d9c759d3a377c
bertoldosi <matheus.bertoldosi@gmail.com>
2022-10-13 18:51:32 -0300

    Refactor: Refatorando

--- src/components/common/Input/index.tsx
+++ src/components/common/Input/index.tsx
@@ -4,36 +4,12 @@ import { focusInput } from "@helpers/focusInput";
 import { Sinput } from "./styles";
 
 type PropsTypes = {
-  name: string;
-  id: string;
-  value?: string | number;
-  onChange?: React.ChangeEventHandler<HTMLInputElement>;
   onKeyUp?: any;
-  disabled?: boolean;
-  type?: string;
-  autofocus?: boolean;
-  tabIndex?: number | undefined;
-  checked?: boolean;
-  placeholder?: string;
-  required?: boolean;
   error?: Object;
+  props: any;
 };
 
-function Input({
-  name,
-  id,
-  value,
-  onChange,
-  disabled,
-  onKeyUp,
-  type,
-  autofocus,
-  tabIndex,
-  checked,
-  placeholder = "Digite um valor",
-  required,
-  error,
-}: PropsTypes) {
+function Input({ error, onKeyUp, ...props }: PropsTypes) {
   const handleOnKeyUp = (event: React.KeyboardEvent<HTMLInputElement>) => {
     const keycode = event.keyCode ? event.keyCode : event.which;
 
@@ -45,21 +21,7 @@ function Input({
 
   return (
     <>
-      <Sinput
-        autoComplete="off"
-        tabIndex={tabIndex}
-        autoFocus={autofocus}
-        name={name}
-        id={id}
-        value={value}
-        placeholder={placeholder}
-        onChange={onChange}
-        onKeyUp={onKeyUp && handleOnKeyUp}
-        disabled={disabled}
-        type={type}
-        checked={checked}
-        required={required}
-      />
+      <Sinput onKeyUp={onKeyUp && handleOnKeyUp} {...props} />
       {error}
     </>
   );
