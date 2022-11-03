43658d1ac9dd8783fc05a519b7643908a80fec54
bertoldosi <matheus.bertoldosi@gmail.com>
2022-11-03 15:33:31 -0300

    feat: Implementando novo header

--- src/components/common/Header/components/DropdownUser/index.tsx
+++ src/components/common/Header/components/DropdownUser/index.tsx
@@ -3,27 +3,32 @@ import { ToggleButtonTheme } from "@commons/ToggleButtonTheme";
 import { User } from "@icons/User";
 import React from "react";
 
-import { Container } from "./styles";
+import { Scontainer, ScontentFooter, Sitem } from "./styles";
 
 export const DropdownUser = () => {
+  const [isVisible, setIsVisible] = React.useState(false);
+
   return (
     <Dropdown
       position="right"
       hideChevronIcon
       icon={<User color="#fff" width={25} height={25} />}
+      isVisible={isVisible}
+      setIsVisible={setIsVisible}
     >
-      <Container>
-        <h1>DropdownMonth</h1>
-        <h1>DropdownMonth</h1>
-        <h1>DropdownMonth</h1>
-        <h1>DropdownMonth</h1>
-        <h1>DropdownMonth</h1>
-        <h1>DropdownMonth</h1>
-        <h1>DropdownMonth</h1>
-        <h1>DropdownMonth</h1>
-        <h1>DropdownMonth</h1>
-        <ToggleButtonTheme />
-      </Container>
+      <Scontainer>
+        <Sitem className="no-emphasis">
+          <h2>Matheus Bertoldo</h2>
+        </Sitem>
+
+        <Sitem>
+          <h2>Sair</h2>
+        </Sitem>
+
+        <ScontentFooter>
+          <ToggleButtonTheme />
+        </ScontentFooter>
+      </Scontainer>
     </Dropdown>
   );
 };
--- src/components/common/Header/components/DropdownUser/styles.ts
+++ src/components/common/Header/components/DropdownUser/styles.ts
@@ -1,5 +1,34 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-export const Container = styled.div`
+export const Scontainer = styled.div`
+  width: 100%;
+  min-width: 20rem;
+`;
 
+export const ScontentFooter = styled.div`
+  height: 5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Sitem = styled.div`
+  padding: 1rem;
+  transition: background 0.5s;
+
+  &:hover {
+    background-color: ${(props) => props.theme.backgroundBory};
+    font-weight: 600;
+  }
+
+  &.no-emphasis {
+    background-color: initial;
+    font-weight: initial;
+    cursor: default;
+  }
+
+  span {
+    color: ${(props) => props.theme.color};
+    font-size: 1.5rem;
+  }
 `;
