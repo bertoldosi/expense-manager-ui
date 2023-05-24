13ee92220a1d7449f241d4c402054174273134b3
bertoldosi <matheus.bertoldosi@gmail.com>
2023-05-24 11:31:28 -0300

    refactor: Refatorando componentes de acesso

--- src/components/container/Login/styles.ts
+++ src/components/container/Login/styles.ts
@@ -1,28 +1,5 @@
 import styled from "styled-components";
 
-export const Slink = styled.span`
-  font-size: 1.5rem;
-
-  a {
-    color: aliceblue;
-  }
-`;
-
-export const Sfooter = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding: 3rem 0 5rem 0;
-`;
-
-export const Sinputs = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 1rem;
-
-  margin-bottom: 1rem;
-`;
-
 export const Sconstainer = styled.div`
   display: flex;
   justify-content: center;
