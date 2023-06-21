2f80ba53a0554a45b21e2389eab7b65abb9e3a91
bertoldosi <matheus.bertoldosi@gmail.com>
2023-06-21 16:56:46 -0300

    feat: Implementando mongoDB

--- src/api/person.ts
+++ src/api/person.ts
@@ -1,7 +1,14 @@
 import { UserType } from "@interfaces/*";
 import instancesInternal from "src/lib/axios-instance-internal";
+import prisma from "../services/prisma";
 
 export const createPerson = async (user: UserType) => {
+  await prisma.user.create({
+    data: {
+      ...user,
+    },
+  });
+
   return instancesInternal.post("/person", user);
 };
 
