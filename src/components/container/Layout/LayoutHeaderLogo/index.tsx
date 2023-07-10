10a7ca743228ad27330a302cb96bb67a0da24ea3
bertoldosi <matheus.bertoldosi@gmail.com>
2023-07-10 14:54:40 -0300

    fix: Removendo wanning do console

--- src/components/container/Layout/LayoutHeaderLogo/index.tsx
+++ src/components/container/Layout/LayoutHeaderLogo/index.tsx
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { ForwardedRef, forwardRef } from "react";
 import Image from "next/image";
 import logoDark from "@assets/logo-dark.svg";
 
 import { Scontainer } from "./styles";
 import Link from "next/link";
 
-function LayoutHeaderLogo() {
-  return (
-    <Scontainer>
-      <Link href="/">
-        <Image src={logoDark} width={120} />
-      </Link>
-    </Scontainer>
-  );
-}
+interface LayoutHeaderLogoProps {}
+
+const LayoutHeaderLogo = forwardRef<HTMLAnchorElement, LayoutHeaderLogoProps>(
+  (props, ref) => {
+    return (
+      <Scontainer>
+        <Link href="/" passHref ref={ref as ForwardedRef<HTMLAnchorElement>}>
+          <a>
+            <Image src={logoDark} width={120} />
+          </a>
+        </Link>
+      </Scontainer>
+    );
+  }
+);
 
 export default LayoutHeaderLogo;
