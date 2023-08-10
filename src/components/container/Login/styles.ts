6b3c95edb97ce668a8d83b4c77899860659bf695
bertoldosi <matheus.bertoldosi@gmail.com>
2023-08-10 13:29:52 -0300

    refactor: Refatorando layout

--- src/components/container/Login/styles.ts
+++ src/components/container/Login/styles.ts
@@ -6,8 +6,12 @@ export const Scontainer = styled.div`
   padding-bottom: 4rem;
 
   > button {
+    height: 4rem;
+    display: flex;
+    align-items: center;
     background-color: ${(props) => props.theme.backgroundSecondaryContrast};
     color: ${(props) => props.theme.textSecondaryContrast};
     padding: 1rem;
+    border-radius: 0.3rem;
   }
 `;
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -33,7 +33,6 @@ export const authOptions: NextAuthOptions = {
         return "/login";
       }
     },
-
     async redirect() {
       return "/";
     },
